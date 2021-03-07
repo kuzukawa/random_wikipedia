bdd9f74f41ae5cd9023b0344db55b61223b7d71e
Keisuke Kuzukawa <kuzukawa@gmail.com>
2021-03-07 11:30:51 +0900

    feat suggest wikipedia article

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const line = require('@line/bot-sdk');
 const express = require('express');
+const axios = require('axios');
 require('dotenv').config();
 
 const config = {
@@ -16,6 +17,8 @@ const client = new line.Client(config);
 const app = express();
 
 app.post('/callback', line.middleware(config), (req, res) => {
+  //console.log(req.body.events);
+
   Promise
     .all(req.body.events.map(handleEvent))
     .then((result) => res.json(result))
@@ -27,18 +30,57 @@ app.post('/callback', line.middleware(config), (req, res) => {
 
 // event handler
 function handleEvent(event) {
-  if(event.typ !== 'message' || event.message.type !== 'text') {
+  if(event.type !== 'message' || event.message.type !== 'text') {
     // ignore non-text-message event
     return Promise.resolve(null);
   }
 
+  const w_arr = ['うぃき', 'ウィキ', 'wiki'];
   // create a text message
-  const echo = { type: 'text', text: event.message.text};
+  if(w_arr.includes(event.message.text)) {
+    getRandomArticle(replyText => {
+     const reply = { type: 'text', text: replyText};
 
-  // use reply API
-  return client.replyMessage(event.replyToken, echo);
+      // use reply API
+      return client.replyMessage(event.replyToken, reply);
+    });
+  }
+  else {
+    //const echo = { type: 'text', text: event.message.text};
+    const reply = { type: 'text', text: 'ごめん！今はまだできないっ👻'};
+
+    // use reply API
+    return client.replyMessage(event.replyToken, reply);
+  }
 }
 
+// get random article from wikipedia
+function getRandomArticle(callback) {
+  const url = 'https://ja.wikipedia.org/w/api.php?format=json&action=query&generator=random&grnnamespace=0&prop=info&inprop=url&indexpageids';
+  
+  axios.get(url)
+    .then(res => {
+      const item = res.data;
+  
+      // 取得したページのIDを取得
+      const pageID = item.query.pageids;
+      // ページのタイトルを取得
+      const pageTitle = item.query.pages[pageID].title;
+      // ページのURLを取得
+      const pageUrl = item.query.pages[pageID].fullurl;
+      
+      // リプライ用のテキスト生成
+      const replyText  = 'こんな記事はどう😆！？'+'\n' + 
+      '【'+pageTitle +'】'+'\n' +
+      pageUrl;
+      
+      callback(replyText);
+    })
+    .catch(err => {
+      callback("ごめん！失敗した！！");
+      console.log(err);
+    })
+}
 // listen on port
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
